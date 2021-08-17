db7b2b3484e711284454b738baa08323fe116714
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-08-17 16:13:44 -0400

    frontend animation file dragging

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 //selecting all required elments
 const dropArea = document.querySelector(".drag-area");
 
+//if user drag file over box
 dropArea.addEventListener("dragover", ()=>{
     console.log("File is over DragArea");
+    dropArea.classList.add("active");
+})
+
+//if user leave drag file from box
+dropArea.addEventListener("dragleave", ()=>{
+    console.log("File is outside DragArea");
+    dropArea.classList.remove("active");
+})
+
+//if user drop file into box
+dropArea.addEventListener("dragleave", ()=>{
+    console.log("File is outside DragArea");
+    dropArea.classList.remove("active");
 })
