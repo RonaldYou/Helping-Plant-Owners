23cfbe5ea85b3c4bfa9fc3d72c7758ef0a397ee5
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-08-17 16:25:12 -0400

    image file dropping

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //selecting all required elments
 const dropArea = document.querySelector(".drag-area");
 
+let file; 
+
 //if user drag file over box
 dropArea.addEventListener("dragover", ()=>{
     console.log("File is over DragArea");
@@ -14,7 +16,20 @@ dropArea.addEventListener("dragleave", ()=>{
 })
 
 //if user drop file into box
-dropArea.addEventListener("dragleave", ()=>{
-    console.log("File is outside DragArea");
+dropArea.addEventListener("drop", (event)=>{
+    event.preventDefault(); //preventing from default behaviour of opening file in new tab
+    console.log("File is dropped into box");
     dropArea.classList.remove("active");
+    //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+    file = event.dataTransfer.files[0];
+    let fileType = file.type;
+    console.log(fileType); //stats of the file
+
+    let validExtensions = ["image/jpeg", "image/png", "image/jpg"];
+    if(validExtensions.includes(fileType)){
+        console.log("This is an image file");
+    }
+    else{
+        alert("this is not an Image File!");
+    }
 })
